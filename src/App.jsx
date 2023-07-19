f9ee2532a9ca6a074b3cc70baccec069e4f9077b
Yehuda Yeger <yeger921@gmail.com>
2023-07-19 12:23:32 +0300

    client up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { socket } from './socket';
+
 
 function App() {
-  return <></>;
+  socket.on("connection",(socket)=> {
+    console.log('Connection');
+    socket.on("disconnect",()=> {
+      console.log('disconnect');
+    })
+  })
+  return <>
+  
+  <p>State: { '' + isConnected }</p>
+  </>;
 }
 
 export default App;
